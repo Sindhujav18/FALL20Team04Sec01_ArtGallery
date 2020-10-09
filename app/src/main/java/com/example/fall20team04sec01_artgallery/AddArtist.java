0d9a0daa5615ccf45497f8e06ecb0f57d1d39f0a
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2020-10-09 15:51:49 -0500

    Added the new activity file

---
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/AddArtist.java
@@ -0,0 +1,14 @@
+package com.example.fall20team04sec01_artgallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddArtist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_artist);
+    }
+}
