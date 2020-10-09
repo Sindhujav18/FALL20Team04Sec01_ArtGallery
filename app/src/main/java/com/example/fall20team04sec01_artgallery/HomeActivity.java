b9fcc5d468b0533f93395e8e9a74b612bf8a7558
Sindhu <sindhujav18@gmail.com>
2020-10-09 18:17:04 -0500

    Created intent to go to home activity and added onclick functionality for navigationview items

--- app/src/main/java/com/example/fall20team04sec01_artgallery/HomeActivity.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/HomeActivity.java
@@ -1,14 +1,35 @@
 package com.example.fall20team04sec01_artgallery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomnavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        bottomnavigationView= findViewById(R.id.navigation);
+        bottomnavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        Intent ini = new Intent(HomeActivity.this, HomeActivity.class);
+                        startActivity(ini);
+                }
+                return true;
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/fall20team04sec01_artgallery/LoginActivity.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.fall20team04sec01_artgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_login);
+        loginBtn = (Button) findViewById(R.id.login_button);
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent loginIntent = new Intent(LoginActivity.this, HomeActivity.class);
+               startActivity(loginIntent);
+
+            }
+        });
     }
 }
