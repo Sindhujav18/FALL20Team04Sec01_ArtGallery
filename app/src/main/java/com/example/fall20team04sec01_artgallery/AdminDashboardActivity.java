f179b4cbbb777ced1766e9fc3678e5c4820b1ca7
sindhu rani goli <saisindhurani@gmail.com>
2020-10-09 21:15:21 -0500

    Added intents for Admin dashboard activity

--- app/src/main/java/com/example/fall20team04sec01_artgallery/AdminDashboardActivity.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/AdminDashboardActivity.java
@@ -3,12 +3,16 @@ package com.example.fall20team04sec01_artgallery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AdminDashboardActivity extends AppCompatActivity {
+    Button addArtist, uploadArt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+        addArtist= findViewById(R.id.addartistBtn);
+
     }
 }
--- app/src/main/java/com/example/fall20team04sec01_artgallery/AdminLogin.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/AdminLogin.java
@@ -2,13 +2,29 @@ package com.example.fall20team04sec01_artgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AdminLogin extends AppCompatActivity {
+    Button adminLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+        adminLogin = findViewById(R.id.admloginBtn);
+
+        adminLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent adminhomeIntent = new Intent(AdminLogin.this, AdminDashboardActivity.class);
+                startActivity(adminhomeIntent);
+            }
+        });
+
+
     }
 }
