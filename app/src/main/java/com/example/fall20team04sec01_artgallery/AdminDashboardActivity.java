4e0c1607159171395e42b76536678c85bf0d7c5d
sindhu rani goli <saisindhurani@gmail.com>
2020-10-09 21:35:14 -0500

    Added intents for Add Artist and Upload Art Buttons.

--- app/src/main/java/com/example/fall20team04sec01_artgallery/AdminDashboardActivity.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/AdminDashboardActivity.java
@@ -2,7 +2,9 @@ package com.example.fall20team04sec01_artgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class AdminDashboardActivity extends AppCompatActivity {
@@ -13,6 +15,21 @@ public class AdminDashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
         addArtist= findViewById(R.id.addartistBtn);
+        uploadArt= findViewById(R.id.uploadartBtn);
+        addArtist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent artistIntent = new Intent(AdminDashboardActivity.this, AddArtist.class);
+                startActivity(artistIntent);
+            }
+        });
+        uploadArt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent uploadartIntent = new Intent(AdminDashboardActivity.this, upload_art.class);
+                startActivity(uploadartIntent);
+            }
+        });
 
     }
 }
