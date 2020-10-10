1358242833e0d28463f96f2fb60aeac9e4219691
Sindhu <sindhujav18@gmail.com>
2020-10-09 19:10:17 -0500

    Created intent for account creation activity

--- app/src/main/java/com/example/fall20team04sec01_artgallery/RegisterActivity.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/RegisterActivity.java
@@ -2,13 +2,27 @@ package com.example.fall20team04sec01_artgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        btn = findViewById(R.id.create_account_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in= new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
