728700ee3b4cd9adbc3e64cac90ad531fb21e4d6
Sindhu <sindhujav18@gmail.com>
2020-10-09 23:48:00 -0500

    Changed some layout files and added intents to GalleryActivity

--- app/src/main/java/com/example/fall20team04sec01_artgallery/HomeActivity.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/HomeActivity.java
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Gallery;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
@@ -13,11 +16,15 @@ import com.google.android.material.navigation.NavigationView;
 public class HomeActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomnavigationView;
+    private Button asiaGallary, africaGallary, europeGallary;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         bottomnavigationView= findViewById(R.id.navigation);
+        asiaGallary= findViewById(R.id.asiaBtn);
+        africaGallary= findViewById(R.id.africaBtn);
+        europeGallary= findViewById(R.id.europeBtn);
         bottomnavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -43,6 +50,26 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-
+       asiaGallary.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent galleryIntent= new Intent(HomeActivity.this, GalleryActivity.class);
+               startActivity(galleryIntent);
+           }
+       });
+        africaGallary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent= new Intent(HomeActivity.this, GalleryActivity.class);
+                startActivity(galleryIntent);
+            }
+        });
+        europeGallary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent= new Intent(HomeActivity.this, GalleryActivity.class);
+                startActivity(galleryIntent);
+            }
+        });
     }
 }
