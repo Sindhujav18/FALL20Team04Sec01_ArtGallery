17ec7f59790ff2cff9a2aca7a9f9f2a59e61b0d6
Sindhu <sindhujav18@gmail.com>
2020-10-09 23:01:44 -0500

    Added intents to PaymentActivity

--- app/src/main/java/com/example/fall20team04sec01_artgallery/AddToBagActivity.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/AddToBagActivity.java
@@ -2,13 +2,26 @@ package com.example.fall20team04sec01_artgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddToBagActivity extends AppCompatActivity {
+
+    Button addtobag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_bag);
+
+        addtobag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bagIntent = new Intent(AddToBagActivity.this, my_cart.class);
+                startActivity(bagIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/fall20team04sec01_artgallery/my_cart.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/my_cart.java
@@ -2,13 +2,25 @@ package com.example.fall20team04sec01_artgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class my_cart extends AppCompatActivity {
-
+    Button confirmBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_cart);
+        confirmBtn = findViewById(R.id.confirm_details);
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent confirmIntent = new Intent(my_cart.this, PaymentActivity.class);
+                startActivity(confirmIntent);
+            }
+        });
+
     }
 }
