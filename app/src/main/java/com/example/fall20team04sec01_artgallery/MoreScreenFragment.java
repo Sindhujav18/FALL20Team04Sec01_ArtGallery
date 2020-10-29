4f21b88c62e6dbdba543f670dfae4beb3584c0f0
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2020-10-29 11:51:58 -0500

    added onclick actions in more screen fragment

--- app/src/main/java/com/example/fall20team04sec01_artgallery/MoreScreenFragment.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/MoreScreenFragment.java
@@ -1,7 +1,13 @@
 package com.example.fall20team04sec01_artgallery;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -31,6 +37,73 @@ public class MoreScreenFragment extends Fragment {
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_more_screen_fragment, container, false);
+        MyListAdapter adapter=new MyListAdapter((Activity) getContext(), maintitle,imgid);
+        list=(ListView)view.findViewById(R.id.MoreScreenList);
+        list.setAdapter(adapter);
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+                // TODO Auto-generated method stub
+                if(position == 0) {
+                    //code specific to first list item
+                    listener.onNavigationRequested(R.layout.activity_user_details);
+                    Toast.makeText(getContext(),"Profile",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(position == 1) {
+                    //code specific to 2nd list item
+                    listener.onNavigationRequested(R.layout.activity_forgot_password);
+                    Toast.makeText(getContext(),"Password",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(position == 2) {
+
+                    listener.onNavigationRequested(R.layout.activity_item_billing_address);
+                    Toast.makeText(getContext(),"Addresses",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 3) {
+
+                    listener.onNavigationRequested(R.layout.activity_payment);
+                    Toast.makeText(getContext(),"Currency",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 4) {
+
+                    listener.onNavigationRequested(R.layout.activity_register);
+                    Toast.makeText(getContext(),"Favourities",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 5) {
+
+                    listener.onNavigationRequested(R.layout.activity_my_cart);
+                    Toast.makeText(getContext(),"Orders",Toast.LENGTH_LONG).show();
+                }
+                else if(position == 6){
+
+                    listener.onNavigationRequested(R.layout.fragment_login);
+                    Toast.makeText(getContext(),"Log Out",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        return view;
     }
 
 }
