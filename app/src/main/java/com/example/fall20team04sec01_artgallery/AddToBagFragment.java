d99334d1675b871c4da02949ad84f3776b1ba94b
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2020-10-28 21:53:48 -0500

    add to bag fragment updated

--- app/src/main/java/com/example/fall20team04sec01_artgallery/AddToBagFragment.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/AddToBagFragment.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -24,5 +25,36 @@ public class AddToBagFragment extends Fragment {
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View inflated = inflater.inflate(R.layout.activity_add_to_bag, container, false);
+
+        addToBag = inflated.findViewById(R.id.adtobagBtn);
+
+        addToBag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onNavigationRequested(R.layout.activity_my_cart);
+            }
+        });
+
+        return inflated;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            listener = (AddToBagFragment.OnNavigationRequestedListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement OnArticleSelectedListener");
+        }
+    }
+
+    public interface OnNavigationRequestedListener {
+        public void onNavigationRequested(int fragmentLayoutId);
     }
 }
