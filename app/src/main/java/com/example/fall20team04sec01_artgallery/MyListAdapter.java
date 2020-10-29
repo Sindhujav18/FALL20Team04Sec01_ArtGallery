d538f2cde752caae71a382d730cf815f37b99b3a
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2020-10-29 12:52:27 -0500

    completed adapter class

--- app/src/main/java/com/example/fall20team04sec01_artgallery/MyListAdapter.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/MyListAdapter.java
@@ -1,7 +1,12 @@
 package com.example.fall20team04sec01_artgallery;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MyListAdapter extends ArrayAdapter<String> {
 
@@ -18,4 +23,18 @@ public class MyListAdapter extends ArrayAdapter<String> {
         this.imgid = imgid;
 
     }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.more_activity_list_view_layout, null,true);
+
+        TextView titleText = (TextView) rowView.findViewById(R.id.title);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+
+        titleText.setText(maintitle[position]);
+        imageView.setImageResource(imgid[position]);
+
+        return rowView;
+
+    };
 }
