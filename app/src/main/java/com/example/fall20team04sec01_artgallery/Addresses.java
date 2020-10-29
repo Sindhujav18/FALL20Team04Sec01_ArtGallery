48f128ea8b9b1e6b0f75a801856de90d3711a8ca
Mohan krishna Alavala <alavalamohan@gmail.com>
2020-10-28 20:57:00 -0500

    addresses class updated

--- app/src/main/java/com/example/fall20team04sec01_artgallery/Addresses.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/Addresses.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,4 +26,19 @@ public class Addresses extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_item_billing_address, container, false);
+    }
+
 }
