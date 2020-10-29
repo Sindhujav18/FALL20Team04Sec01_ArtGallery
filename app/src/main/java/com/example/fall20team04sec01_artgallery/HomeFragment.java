71c1a7623e2641370b2f2ca6c37971c3724dc2fe
Mohan krishna Alavala <alavalamohan@gmail.com>
2020-10-28 22:51:10 -0500

    home fragment created

--- app/src/main/java/com/example/fall20team04sec01_artgallery/HomeFragment.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.fall20team04sec01_artgallery;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,10 +12,29 @@ import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeFragment extends Fragment {
+    public HomeFragment() {
+        // Required empty public constructor
+    }
 
-    private BottomNavigationView bottomnavigationView;
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     private Button asiaGallary, africaGallary, europeGallary;
+    private OnNavigationRequestedListener listener;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,19 +48,19 @@ public class HomeActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.navigation_home:
-                        Intent ini = new Intent(HomeActivity.this, HomeActivity.class);
+                        Intent ini = new Intent(HomeFragment.this, HomeFragment.class);
                         startActivity(ini);
                         break;
                     case R.id.navigation_cart:
-                        Intent cintent = new Intent(HomeActivity.this, my_cart.class);
+                        Intent cintent = new Intent(HomeFragment.this, my_cart.class);
                         startActivity(cintent);
                         break;
                     case R.id.navigation_dashboard:
-                        Intent categoryIntent = new Intent(HomeActivity.this, CategoryFragment.class);
+                        Intent categoryIntent = new Intent(HomeFragment.this, CategoryFragment.class);
                         startActivity(categoryIntent);
                         break;
                     case R.id.navigation_search:
-                        Intent searchIntent = new Intent(HomeActivity.this, SearchActivity.class);
+                        Intent searchIntent = new Intent(HomeFragment.this, SearchActivity.class);
                         startActivity(searchIntent);
                         break;
                 }
@@ -51,21 +71,21 @@ public class HomeActivity extends AppCompatActivity {
        asiaGallary.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
-               Intent galleryIntent= new Intent(HomeActivity.this, GalleryFragment.class);
+               Intent galleryIntent= new Intent(HomeFragment.this, GalleryFragment.class);
                startActivity(galleryIntent);
            }
        });
         africaGallary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent galleryIntent= new Intent(HomeActivity.this, GalleryFragment.class);
+                Intent galleryIntent= new Intent(HomeFragment.this, GalleryFragment.class);
                 startActivity(galleryIntent);
             }
         });
         europeGallary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent galleryIntent= new Intent(HomeActivity.this, GalleryFragment.class);
+                Intent galleryIntent= new Intent(HomeFragment.this, GalleryFragment.class);
                 startActivity(galleryIntent);
             }
         });
