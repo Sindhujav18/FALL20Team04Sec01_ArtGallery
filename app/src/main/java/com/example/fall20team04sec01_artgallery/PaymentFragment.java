ccf93fad39d6c3d611bee6edfb99b1ee10cb4f19
Mohan krishna Alavala <alavalamohan@gmail.com>
2020-10-29 13:46:01 -0500

    completed payment fragment

--- app/src/main/java/com/example/fall20team04sec01_artgallery/PaymentFragment.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/PaymentFragment.java
@@ -1,9 +1,14 @@
 package com.example.fall20team04sec01_artgallery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class PaymentFragment extends Fragment {
     OnNavigationRequestedListener listener;
@@ -23,6 +28,27 @@ public class PaymentFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View inflated = inflater.inflate(R.layout.activity_payment, container, false);
+
+
+
+        return inflated;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            listener = (OnNavigationRequestedListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement OnArticleSelectedListener");
         }
     }
 
