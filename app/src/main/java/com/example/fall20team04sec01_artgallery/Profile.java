39052020b8608a4c785868536ea6f1598a737157
Mohan krishna Alavala <alavalamohan@gmail.com>
2020-10-29 13:54:11 -0500

    profile fragment created

---
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/Profile.java
@@ -0,0 +1,20 @@
+package com.example.fall20team04sec01_artgallery;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Profile#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Profile extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    public Profile() {
+        // Required empty public constructor
+    }
+}
