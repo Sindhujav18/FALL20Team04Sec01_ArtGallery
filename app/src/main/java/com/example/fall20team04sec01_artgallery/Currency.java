e80ff57a107f1ccb2cdfd0a10293030e04435555
sindhu rani goli <saisindhurani@gmail.com>
2020-10-28 22:37:05 -0500

    Added code to Currency Fragment

--- app/src/main/java/com/example/fall20team04sec01_artgallery/Currency.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/Currency.java
@@ -1,4 +1,31 @@
 package com.example.fall20team04sec01_artgallery;
 
-public class Currency {
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Currency#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Currency extends Fragment {
+
+    public Currency() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static Currency newInstance() {
+        Currency fragment = new Currency();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
 }
