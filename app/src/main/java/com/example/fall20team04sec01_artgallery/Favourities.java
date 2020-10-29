147712e066536721d4904833b1fa46668a5d7012
sindhu rani goli <saisindhurani@gmail.com>
2020-10-28 23:25:35 -0500

    Updated code to Favourities Fragment

--- app/src/main/java/com/example/fall20team04sec01_artgallery/Favourities.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/Favourities.java
@@ -23,4 +23,17 @@ public class Favourities extends Fragment {
         return fragment;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_register, container, false);
+    }
 }
