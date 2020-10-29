e42d0a436b93276b06063a163619200b671f58da
Mohan krishna Alavala <alavalamohan@gmail.com>
2020-10-29 13:36:49 -0500

    Added code to payment fragment

--- app/src/main/java/com/example/fall20team04sec01_artgallery/PaymentFragment.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/PaymentFragment.java
@@ -1,14 +1,32 @@
 package com.example.fall20team04sec01_artgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-public class PaymentFragment extends AppCompatActivity {
+public class PaymentFragment extends Fragment {
+    OnNavigationRequestedListener listener;
+
+    public PaymentFragment() {
+        // Required empty public constructor
+    }
+
+    public static PaymentFragment newInstance() {
+        PaymentFragment fragment = new PaymentFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment);
+        if (getArguments() != null) {
+        }
+    }
+
+    public interface OnNavigationRequestedListener {
+        public void onNavigationRequested(int fragmentLayoutId);
     }
 }
