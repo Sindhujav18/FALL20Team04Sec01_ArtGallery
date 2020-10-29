c45d08ede2c8933f354c4ad63f8fa052c8e6f633
Sindhu <sindhujav18@gmail.com>
2020-10-29 00:51:14 -0500

    Modified data access object interface

--- app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/DAO.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/DAO.java
@@ -1,4 +1,16 @@
 package com.example.fall20team04sec01_artgallery.RoomDatabase;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+
+@Dao
 public interface DAO {
+
+    @Insert(onConflict =  OnConflictStrategy.FAIL)
+    public Long UserInsertion(UserModel userModel);
 }
