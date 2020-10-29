198e45e101e8d3cec571b0cff27673e0c700ded4
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2020-10-29 12:36:13 -0500

    added code

--- app/src/main/java/com/example/fall20team04sec01_artgallery/MyCartFragment.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/MyCartFragment.java
@@ -1,11 +1,15 @@
 package com.example.fall20team04sec01_artgallery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 public class MyCartFragment extends Fragment {
@@ -31,6 +35,34 @@ public class MyCartFragment extends Fragment {
     }
 
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View inflated = inflater.inflate(R.layout.activity_my_cart, container, false);
 
+        confirmBtn = inflated.findViewById(R.id.confirm_details);
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onNavigationRequested(R.layout.activity_payment);
+            }
+        });
+
+        return inflated;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            listener = (MyCartFragment.OnNavigationRequestedListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement OnArticleSelectedListener");
+        }
+    }
+
+    public interface OnNavigationRequestedListener {
+        public void onNavigationRequested(int fragmentLayoutId);
     }
 }
