376b4e4aabf9aefeb29714cf88ad25931496318a
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2020-10-28 22:06:16 -0500

    added code to the category fragment

--- app/src/main/java/com/example/fall20team04sec01_artgallery/CategoryFragment.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/CategoryFragment.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 /**
@@ -23,4 +26,19 @@ public class CategoryFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_categories, container, false);
+    }
+
 }
