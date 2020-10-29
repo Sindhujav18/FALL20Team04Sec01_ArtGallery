d03e4e8f073cdbdb1c1781256e22dfd7151b8580
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2020-10-29 13:32:34 -0500

    payment fragment created

--- app/src/main/java/com/example/fall20team04sec01_artgallery/PaymentFragment.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/PaymentFragment.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PaymentActivity extends AppCompatActivity {
+public class PaymentFragment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
