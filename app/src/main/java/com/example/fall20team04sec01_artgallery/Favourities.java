64c0fa50d9915da971a20fe534a470f83050a7b1
sindhu rani goli <saisindhurani@gmail.com>
2020-10-28 23:13:25 -0500

    Added code to Favourities Fragment

--- app/src/main/java/com/example/fall20team04sec01_artgallery/Favourities.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/Favourities.java
@@ -1,4 +1,26 @@
 package com.example.fall20team04sec01_artgallery;
 
-public class Favourities {
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Favourities extends Fragment {
+
+
+
+    public Favourities() {
+        // Required empty public constructor
+    }
+
+    public static Favourities newInstance() {
+        Favourities fragment = new Favourities();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
 }
