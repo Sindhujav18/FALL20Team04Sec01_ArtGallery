d8285fee61c0418e277f8e7f47d992eb6353a07f
Sindhu <sindhujav18@gmail.com>
2020-10-30 14:26:28 -0500

    Password fragment has been completed successfully

--- app/src/main/java/com/example/fall20team04sec01_artgallery/Password.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/Password.java
@@ -1,6 +1,9 @@
 package com.example.fall20team04sec01_artgallery;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,6 +19,20 @@ public class Password extends Fragment {
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_forgot_password, container, false);
     }
 }
 
