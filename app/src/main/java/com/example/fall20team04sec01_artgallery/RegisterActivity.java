8256f3811301771cea3f72d729283acea5f138eb
Mohan krishna Alavala <alavalamohan@gmail.com>
2020-10-30 14:57:28 -0500

    delared variables and added id in RegisterActivity

--- app/src/main/java/com/example/fall20team04sec01_artgallery/RegisterActivity.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/RegisterActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText name,email,phoneNumber,password;
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +19,13 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         btn = findViewById(R.id.create_account_button);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent in= new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(in);
-            }
-        });
+        name = findViewById(R.id.name_edit_text);
+        email = findViewById(R.id.email_edit_text);
+        phoneNumber = findViewById(R.id.phone_edit_text);
+        password = findViewById(R.id.r_password_edit_text);
+
+        final String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
     }
 }
