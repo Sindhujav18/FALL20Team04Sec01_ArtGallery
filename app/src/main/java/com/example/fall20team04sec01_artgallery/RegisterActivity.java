e412ef52d9a2b7a6c74cb771dad2a18476b3b834
Sindhu <sindhujav18@gmail.com>
2020-10-30 15:06:11 -0500

    RegisterActivity completed successfully

--- app/src/main/java/com/example/fall20team04sec01_artgallery/RegisterActivity.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/RegisterActivity.java
@@ -1,13 +1,18 @@
 package com.example.fall20team04sec01_artgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.fall20team04sec01_artgallery.RoomDatabase.MyDatabase;
+import com.example.fall20team04sec01_artgallery.RoomDatabase.UserModel;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -25,6 +30,39 @@ public class RegisterActivity extends AppCompatActivity {
         password = findViewById(R.id.r_password_edit_text);
 
         final String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(name.getText().length()<=4)
+                    Toast.makeText(getApplicationContext(),"Name is too short",Toast.LENGTH_LONG).show();
+                else if(!(email.getText().toString().trim().matches(emailPattern)))
+                    Toast.makeText(getApplicationContext(),"Invalid Email",Toast.LENGTH_LONG).show();
+                else if(phoneNumber.getText().length()<=10)
+                    Toast.makeText(getApplicationContext(),"Invalid Phone Number\nDigits should be greater than 10",Toast.LENGTH_LONG).show();
+                else if(password.getText().length()<=5)
+                    Toast.makeText(getApplicationContext(),"Week Password\nPassword length should be greater than 5",Toast.LENGTH_LONG).show();
+                else {
+                    UserModel user = new UserModel(name.getText().toString(), email.getText().toString(), phoneNumber.getText().toString(), password.getText().toString());
+
+                    MyDatabase myDatabase = Room.databaseBuilder(RegisterActivity.this, MyDatabase.class, "UserDb")
+                            .allowMainThreadQueries().build();
+
+                    try {
+
+                        Long message = myDatabase.dao().UserInsertion(user);
+                        Log.e("DB message : ",message+"");
+                        Intent in = new Intent(RegisterActivity.this, LoginActivity.class);
+                        startActivity(in);
+                    }
+                    catch (Exception database){
+
+                        Toast.makeText(getApplicationContext(),"You already have an a Account",Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
 
 
     }
