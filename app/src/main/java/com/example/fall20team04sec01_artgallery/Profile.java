bc05ec7247665758fd6ab21ae5f687a8d8bd81ef
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2020-10-30 14:07:51 -0500

    profile completed

--- app/src/main/java/com/example/fall20team04sec01_artgallery/Profile.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/Profile.java
@@ -1,5 +1,10 @@
 package com.example.fall20team04sec01_artgallery;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
 /**
@@ -16,5 +21,26 @@ public class Profile extends Fragment {
 
     public Profile() {
         // Required empty public constructor
+    }
+
+    public static Profile newInstance() {
+        Profile fragment = new Profile();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_user_details, container, false);
     }
 }
