f8fead6a8589f0ce45d951e27fd1f1202e8922ea
Mohan krishna Alavala <alavalamohan@gmail.com>
2020-11-19 17:57:36 -0600

    forgot password class created

--- app/src/main/java/com/example/fall20team04sec01_artgallery/ForgotPassword/ForgotPassword.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/ForgotPassword/ForgotPassword.java
@@ -1,4 +1,26 @@
 package com.example.fall20team04sec01_artgallery.ForgotPassword;
 
-public class ForgotPassword {
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fall20team04sec01_artgallery.R;
+import com.example.fall20team04sec01_artgallery.RoomDatabase.MyDatabase;
+
+public class ForgotPassword extends AppCompatActivity {
+
+
+    EditText emailEditText;
+
+    MyDatabase myDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        emailEditText = findViewById(R.id.email_edit_text);
+    }
 }
+
