d8626b85eccabb154b0005ec2e75eadecba7e0b7
Sindhu <sindhujav18@gmail.com>
2020-11-19 18:24:56 -0600

    added setters and getters for admin credentials

--- app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/AdminCredentials.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/AdminCredentials.java
@@ -17,4 +17,19 @@ public class AdminCredentials {
         this.email = email;
         this.password = password;
     }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
