81061f10ab057083717657455ac72cf65974bb41
Sindhu <sindhujav18@gmail.com>
2020-11-19 18:39:50 -0600

    Added required methods in artistmodel class

--- app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/ArtistModel.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/ArtistModel.java
@@ -19,4 +19,36 @@ public class ArtistModel {
         this.phoneNumber = phoneNumber;
         this.country = country;
     }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
+
