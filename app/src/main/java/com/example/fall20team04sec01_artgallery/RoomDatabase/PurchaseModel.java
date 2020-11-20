e1e7b03c88454b76e467e61b93b8acb3655cdfd1
sindhu rani goli <saisindhurani@gmail.com>
2020-11-19 19:27:20 -0600

    Variables added

--- app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/PurchaseModel.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/PurchaseModel.java
@@ -1,4 +1,23 @@
 package com.example.fall20team04sec01_artgallery.RoomDatabase;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(foreignKeys = {
+        @ForeignKey(entity = Art.class, parentColumns = "id", childColumns = "artId"),
+        @ForeignKey(entity = UserModel.class, parentColumns = "email", childColumns = "userEmailId")
+})
 public class PurchaseModel {
-}
+    @PrimaryKey
+    Integer _id;
+
+    @ColumnInfo(name = "artId")
+    Integer artId;
+
+    @ColumnInfo(name = "userEmailId")
+    String userEmailId;
+
+    @ColumnInfo(name = "quantity")
+    Integer quantity;
