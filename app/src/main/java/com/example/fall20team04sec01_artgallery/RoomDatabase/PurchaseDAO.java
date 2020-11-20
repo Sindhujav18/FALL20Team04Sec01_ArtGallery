0fbdd372f2e246483de25ccf884361482420cf7d
sindhu rani goli <saisindhurani@gmail.com>
2020-11-19 19:34:20 -0600

    Added code to PurchaseDAO

--- app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/PurchaseDAO.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/PurchaseDAO.java
@@ -1,4 +1,17 @@
 package com.example.fall20team04sec01_artgallery.RoomDatabase;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface PurchaseDAO {
+    @Insert(onConflict = OnConflictStrategy.FAIL)
+    public long PurchaseInsertion(PurchaseModel data);
+
+    @Query("SELECT * FROM Art WHERE Art.id IN (SELECT PurchaseModel.artId FROM PurchaseModel WHERE PurchaseModel.userEmailId = :email)")
+    public List<Art> getUsersPurchases(String email);
 }
