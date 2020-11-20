4c4d4771aeec6afbff122be1fc71db8c2f6da222
sindhu rani goli <saisindhurani@gmail.com>
2020-11-19 19:21:05 -0600

    Code is added to FavouriteDAO

--- app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/FavouriteDAO.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/FavouriteDAO.java
@@ -1,5 +1,23 @@
 package com.example.fall20team04sec01_artgallery.RoomDatabase;
 
-public interface FavouriteDAO {
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 
+import java.util.List;
+
+@Dao
+public interface FavouriteDAO {
+    @Insert(onConflict = OnConflictStrategy.FAIL)
+    public void makeFavourite(FavouriteModel data);
+
+    @Query("DELETE FROM FavouriteModel WHERE FavouriteModel.artId = :artId AND FavouriteModel.userEmailId = :userId")
+    public void removeFavourite(Integer artId, String userId);
+
+    @Query("SELECT * FROM Art WHERE Art.id IN (SELECT FavouriteModel.artId FROM FavouriteModel WHERE FavouriteModel.userEmailId = :email)")
+    public List<Art> getUsersFavourites(String email);
+
+    @Query("SELECT * FROM PurchaseModel")
+    public List<FavouriteModel> getAll();
 }
