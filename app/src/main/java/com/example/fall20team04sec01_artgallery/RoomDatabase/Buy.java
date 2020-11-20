ad4ea5fb0d504921854fad7c30abc23b9bb8f6a1
Sindhu <sindhujav18@gmail.com>
2020-11-19 18:46:23 -0600

    Added getters and setter methods for item purchased details

--- app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/Buy.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/Buy.java
@@ -24,4 +24,36 @@ public class Buy {
         this.buy_date = buy_date;
         this.quantity = quantity;
     }
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getArtId() {
+        return artId;
+    }
+
+    public void setArtId(int artId) {
+        this.artId = artId;
+    }
+
+    public String getBuy_date() {
+        return buy_date;
+    }
+
+    public void setBuy_date(String buy_date) {
+        this.buy_date = buy_date;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
+
