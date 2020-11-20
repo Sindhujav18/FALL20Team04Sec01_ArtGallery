ffa3f50223828d4ee1ee0f8646311c52a6b02011
sindhu rani goli <saisindhurani@gmail.com>
2020-11-19 19:17:26 -0600

    Variables added in FavouriteModel.java

--- app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/FavouriteModel.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/FavouriteModel.java
@@ -1,4 +1,20 @@
 package com.example.fall20team04sec01_artgallery.RoomDatabase;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(foreignKeys = {
+        @ForeignKey(entity = Art.class, parentColumns = "id", childColumns = "artId"),
+        @ForeignKey(entity = UserModel.class, parentColumns = "email", childColumns = "userEmailId")
+})
 public class FavouriteModel {
-}
+    @PrimaryKey
+    Integer _id;
+
+    @ColumnInfo(name = "artId")
+    Integer artId;
+
+    @ColumnInfo(name = "userEmailId")
+    String userEmailId;
