d1d4e621cd49bdc428ccff282731c48fe538c4e5
Mohan krishna Alavala <alavalamohan@gmail.com>
2020-11-20 20:25:56 -0600

    add artist code has been added

--- app/src/main/java/com/example/fall20team04sec01_artgallery/AddArtist.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/AddArtist.java
@@ -1,14 +1,66 @@
 package com.example.fall20team04sec01_artgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.fall20team04sec01_artgallery.RoomDatabase.ArtistModel;
+import com.example.fall20team04sec01_artgallery.RoomDatabase.MyDatabase;
 
 public class AddArtist extends AppCompatActivity {
+
+    EditText email,name,phoneNumber,country;
+
+    MyValidator validator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_artist);
+
+        email = findViewById(R.id.email_edit_text);
+        name = findViewById(R.id.Address);
+        phoneNumber = findViewById(R.id.contact_number_edit_text);
+        country = findViewById(R.id.country_edit_text);
+
+        validator = new MyValidator(getApplicationContext());
+
+
+        findViewById(R.id.done_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(validator.nameValidate(name.getText().toString()));
+                else if(validator.emailValidate(email.getText().toString()));
+                else if(validator.phoneNumberValidate(phoneNumber.getText().toString()));
+                else if(validator.countryValidate(country.getText().toString()));
+                else {
+
+                    ArtistModel artist = new ArtistModel(name.getText().toString().toLowerCase(), email.getText().toString(), phoneNumber.getText().toString(), country.getText().toString());
+
+                    MyDatabase myDatabase = Room.databaseBuilder(AddArtist.this, MyDatabase.class, "UserDb")
+                            .allowMainThreadQueries().build();
+
+                    try {
+
+                        Long message = myDatabase.dao().ArtistInsertion(artist);
+                        Log.e("DB message : ",message+"");
+                        Toast.makeText(getApplicationContext(),"Artist Added",Toast.LENGTH_SHORT).show();
+                        //startActivity(in);
+                    }
+                    catch (Exception database){
+
+                        Toast.makeText(getApplicationContext(),"You already have an a Account",Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+
+        });
     }
 }
