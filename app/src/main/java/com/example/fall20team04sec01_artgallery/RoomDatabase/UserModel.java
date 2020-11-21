178df6db1e564102540085cea899286170789160
Sindhu <sindhujav18@gmail.com>
2020-11-20 22:26:14 -0600

    bugs fixed

--- app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/UserModel.java
+++ app/src/main/java/com/example/fall20team04sec01_artgallery/RoomDatabase/UserModel.java
@@ -2,21 +2,28 @@ package com.example.fall20team04sec01_artgallery.RoomDatabase;
 
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
 
-@Entity(primaryKeys = {"email" })
+
+@Entity(indices = {@Index(value = {"email"}, unique = true)})
 public class UserModel {
+
+    @PrimaryKey(autoGenerate = true)
     int id;
 
     @NonNull
     String email;
 
-    String name,phoneNumber,password;
 
-    public UserModel(String name,String email, String phoneNumber, String password) {
+    String name,phoneNumber,password,address;
+
+    public UserModel(String name,String email, String phoneNumber, String password,String address) {
         this.email = email;
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.password = password;
+        this.address = address;
     }
     public int getId() {
         return id;
@@ -57,4 +64,13 @@ public class UserModel {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
 }
